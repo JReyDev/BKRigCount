d3cad7ff21ecaad52a4fc173835bad5af87c7df6
JReyDev <102834451+JReyDev@users.noreply.github.com>
2023-08-29 22:47:44 -0700

    Update BakerHughesRigCount.py

--- BakerHughesRigCount.py
+++ BakerHughesRigCount.py
@@ -26,7 +26,7 @@ def fetch_and_process_rig_data(url, save_path, sheet_name):
 
 if __name__ == '__main__':
     url = 'https://rigcount.bakerhughes.com/static-files/e916080f-555e-4e18-9969-46f62fbef9d6'
-    save_path = 'F:/Downloads/north_america_rotary_rig_count_jan_2000_-_current.xlsb'
+    save_path = 'download/file.xlsb'
     sheet_name = 'US Oil & Gas Split'
 
     rig_df = fetch_and_process_rig_data(url, save_path, sheet_name)
