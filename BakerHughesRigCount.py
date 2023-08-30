46e7a2ab5c639e0079171b9277db7eb79a6a6c67
JReyDev <102834451+JReyDev@users.noreply.github.com>
2023-08-29 23:02:05 -0700

    Update BakerHughesRigCount.py

--- BakerHughesRigCount.py
+++ BakerHughesRigCount.py
@@ -2,32 +2,28 @@ import requests
 import xlrd
 import pandas as pd
 import os
+from io import BytesIO
 
-def fetch_and_process_rig_data(url, save_path, sheet_name):
+def fetch_and_process_rig_data(url ,sheet_name):
     # Fetch data
     dl = requests.get(url)
     
-    # Save to disk
-    with open(save_path, 'wb') as rig_data:
-        rig_data.write(dl.content)
-    try:
+    if dl.status_code == 200:
+        # Read the Excel file into a Pandas DataFrame
+        with BytesIO(dl.content) as bio:
+            rig_count = pd.read_excel(bio, engine='pyxlsb', sheet_name='US Oil & Gas Split')
 
-        # Read and clean up Excel data"
-        rig_count = pd.read_excel(save_path, engine='pyxlsb', sheet_name=sheet_name)
         rig_df = pd.DataFrame(rig_count[5:])
         rig_df.columns = rig_df.iloc[0]
         rig_df = rig_df.iloc[1:].reset_index(drop=True)
         rig_df['Date'] = rig_df['Date'].apply(lambda x: xlrd.xldate_as_datetime(x, 0))
     
-    finally:
-        os.remove(save_path)
 
     return rig_df
 
 if __name__ == '__main__':
-    url = 'https://rigcount.bakerhughes.com/static-files/othernumbers'
-    save_path = 'download/file.xlsb'
+    url = 'https://rigcount.bakerhughes.com/static-files/e916080f-555e-4e18-9969-46f62fbef9d6'
     sheet_name = 'US Oil & Gas Split'
 
-    rig_df = fetch_and_process_rig_data(url, save_path, sheet_name)
+    rig_df = fetch_and_process_rig_data(url, sheet_name)
     print(rig_df.head())
